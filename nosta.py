d617d87f058b2920de7e54e99346257067ee28eb
ArionHill <yngbhu@gmail.com>
2018-03-30 09:28:39 +0800

    add commit

--- nosta.py
+++ nosta.py
@@ -9,6 +9,7 @@ ROOT = '/home/hill/Documents/nosta_crawler/pages/'
 
 def get_page(url):
     url = url.replace('\'', '')
+    print 'get_page: ', url
     content = dict()
     path = ROOT + url
     if os.path.exists(path):
@@ -21,7 +22,7 @@ def get_page(url):
     requests_num = 0
 
 
-    while requests_num < 100:
+    while requests_num < 10:
         try:
             request = urllib2.Request(url, headers=headers)
             response = urllib2.urlopen(request, timeout=10)
@@ -36,57 +37,44 @@ def get_page(url):
                 os.makedirs(dr)
             with open(path, 'w') as fp:
                 fp.write(content[url])
-
+            print 'done'
             return content
         except Exception as e:
             requests_num += 1
             print url
             if requests_num == 100:
                 raise e
-            time.sleep(10*requests_num)
+            time.sleep(2**requests_num)
 
 
 def get_links(page):
+    print 'get_links'
     links = []
     for p, content in page.items():
+        # *('.*href=(.*) target.*')是贪婪匹配，在不影响后续匹配是否成功的前提下*会一直匹配下去，所以只能找到一行中的最后一个字符串;
+        # 改为非贪婪的*？('.*?href=(.*?) target.*?')就可以找到到所有符合条件的字符串
         link_pattern = '.*?href=(.*?) target.*?'
         links_on_p = re.findall(link_pattern, content)
-        print links_on_p
-        # with open('page.html', 'w') as fp:
-        #     print type(content)
-        #     fp.write(content)
-        # <IMG src='pic/title.jpg'>
-        # print repr(content)
-        # img_pattern = '.*IMG src=\'(.*)jpg\'.*'
-        # img_on_p1 = re.findall(img_pattern, content)
-        # print img_on_p1
-        # img_pattern = '.*IMG src=\'(.*)jpg\'>.*'
-        # img_on_p2 = re.findall(img_pattern, content)
-        # print img_on_p2
-        # # ><IMG src='../pic/000-2001/4-1.jpg' width='840px'>
         img_pattern = '.*?IMG src=\'(.*?)jpg\'.*?'
         img_on_p = re.findall(img_pattern, content, re.M)
-        print img_on_p
-        # img_on_p = img_on_p1 + img_on_p2 + img_on_p3
-        # print img_on_p
-
         for img in img_on_p:
             links.append(os.path.join(os.path.split(p)[0],
                                       img + 'jpg'.replace('\'', '')))
         for link in links_on_p:
             links.append(os.path.join(os.path.split(p)[0],
                                       link.replace('\'', '')))
+    print 'done'
     return links
 
 
 if __name__ == '__main__':
     root = '/home/hill/Documents/nosta_crawler/pages/'
     url = 'http://www.nosta.gov.cn/upload/2018slxmgb/showProject.html'
-    # url = 'http://www.nosta.gov.cn/upload/2018slxmgb/zr_101/zrIndex.html'
-    # url = 'http://www.nosta.gov.cn/upload/2018slxmgb/zr_101/000-2001.html'
+
     links = [url]
     i = 0
     while links != []:
+        print 'links != []'
         for link in links:
             with open('links.txt', 'a+') as fp:
                 fp.write('    ' * i + link + '\n')
@@ -96,5 +84,6 @@ if __name__ == '__main__':
             pages.append(get_page(url))
         links = []
         for page in pages:
+            print 'page'
             links.extend(get_links(page))
-    print(links)
+    print 'DONE'
